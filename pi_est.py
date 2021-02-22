7a781e664ce19ede0cadb7d90e36a2c28bba846e
Hrithik Joseph <31767239+HrithikMJ@users.noreply.github.com>
2021-02-22 12:28:26 +0530

    Add files via upload

---
+++ pi_est.py
@@ -0,0 +1,20 @@
+'''
+A pi estimator inspired from joma
+Watch his video here https://www.youtube.com/watch?v=pvimAM_SLic
+'''
+import random
+
+def est_pi(n):
+    pts=0
+    pts_c=0
+    for i in range(n):
+            x=random.random()
+            y=random.random()
+            dist=x**2+y**2
+            if dist<=1:
+                pts_c+=1
+            pts+=1
+    print(4*pts_c/pts)
+
+_=int(input())
+est_pi(_)
